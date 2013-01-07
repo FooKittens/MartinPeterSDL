fd212cafa28beb1e69ac9ff0b3dd9038a6e1fb64
Peter Mårtensson <veritas.is.me@gmail.com>
2013-01-07 19:53:16 +0100

    Added SDL stuff, fixed import paths
    Signed-off-by: Peter Mårtensson <veritas.is.me@gmail.com>

---
+++ MartinPeterSDL/Globals.h
@@ -0,0 +1,15 @@
+#include "include\SDL.h"
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#define NULL 0
+
+
+SDL_Surface* g_Buffer = NULL;
+
+const int g_ScreenWidth = 800;
+const int g_ScreenHeight = 600;
+const int g_BitDepth = 32;
+const int g_DelayTime = 16;
+#endif
--- MartinPeterSDL/main.cpp
+++ MartinPeterSDL/main.cpp
@@ -1,8 +1,75 @@
 #include <iostream>
 #include "SDL.h"
+#include "Globals.h"
+SDL_Surface* buffer = NULL;
+
+SDL_Event myEvent;
+
+int Init();
+int FlipAndClear();
 
 int main(int argc, char* argv[])
 {
+  bool running = true;
+
+  int x = 64;
+
+  while(running)
+  {
+    
+    SDL_Rect rect = {x, 64, 64, 64};
+    SDL_FillRect(buffer, &rect, 0xFF0000); 
+
+    Uint8* keys = SDL_GetKeyState(NULL);
+    if(keys[SDLK_x])
+    {
+        x += 4;
+    }
+
+    while(SDL_PollEvent(&myEvent))
+    {
+      if(myEvent.type == SDL_QUIT)
+      {
+        running = false;
+      }
+      else if(myEvent.key.keysym.sym == SDLK_ESCAPE)
+      {
+        running = false;
+      }
+    }
+
+    
+
+    SDL_Delay(16);
+    
+  }
+
+  SDL_FreeSurface(buffer);
+  SDL_Quit();
 
   return 0;
+}
+
+int Init()
+{
+  SDL_Init(SDL_INIT_EVERYTHING);
+  buffer = SDL_SetVideoMode(
+    g_ScreenWidth,
+    g_ScreenHeight,
+    g_BitDepth,
+    SDL_SWSURFACE
+  );
+
+  if(buffer == NULL)
+  {
+    return 1;
+  }
+
+  return 0;
+}
+
+int FlipAndClear()
+{
+  SDL_Flip(buffer);
+  SDL_FillRect(buffer, NULL, 0);
 }
